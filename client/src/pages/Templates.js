3a9d16e58f7e51266d3d038d25cd5181c76e9367
indrasensingh823 <indrasensingh823gkp@gmail.com>
2025-06-14 06:47:41 +0530

    Remove unused useState import to fix build error

--- client/src/pages/Templates.js
+++ client/src/pages/Templates.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import Quill from 'quill';
 import html2pdf from 'html2pdf.js';
 import 'quill/dist/quill.snow.css';
